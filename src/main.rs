56bf3eab33e905509a0e1b06af5804bcbaa320fb
Artem Kostin <artem.1.kostin@gmail.com>
2023-02-18 15:43:40 -0500

    Port to stm32f303

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,22 @@
 #![no_main]
 
 use panic_halt as _;
-use stm32f4::stm32f405::{self, interrupt};
+pub use cortex_m_rt::entry;
+use stm32f3::stm32f303::{self, interrupt};
 
 #[cortex_m_rt::entry]
 fn start() -> ! {
     // Acquire the device peripherals. They can only be taken once ever.
-    let device_peripherals = stm32f405::Peripherals::take().unwrap();
+    let device_peripherals = stm32f303::Peripherals::take().unwrap();
 
     // Get a reference to GPIOA and RCC to save typing.
-    let gpioa = &device_peripherals.GPIOA;
+    let gpioe = &device_peripherals.GPIOE;
     let rcc = &device_peripherals.RCC;
     let tim2 = &device_peripherals.TIM2;
 
     // Enable the GPIOA clock and set PA8 to be an output
-    rcc.ahb1enr.modify(|_, w| w.gpioaen().enabled());
-    gpioa.moder.modify(|_, w| w.moder8().output());
+    rcc.ahbenr.modify(|_, w| w.iopeen().enabled());
+    gpioe.moder.modify(|_, w| w.moder8().output());
 
     // Set up the timer for slow interrupt generation
     // NOTE(unsafe): The psc field has not been sufficiently documented
@@ -29,7 +30,7 @@ fn start() -> ! {
     tim2.cr1.write(|w| w.cen().enabled());
 
     // Enable the timer interrupt in the NVIC.
-    unsafe { cortex_m::peripheral::NVIC::unmask(stm32f405::Interrupt::TIM2) };
+    unsafe { cortex_m::peripheral::NVIC::unmask(stm32f303::Interrupt::TIM2) };
 
     // The main thread can now go to sleep.
     // WFI (wait for interrupt) puts the core in sleep until an interrupt occurs.
@@ -50,12 +51,12 @@ fn TIM2() {
     // https://rust-embedded.github.io/book/concurrency/concurrency.html
 
     // Clear the UIF bit to indicate the interrupt has been serviced
-    unsafe { (*stm32f405::TIM2::ptr()).sr.modify(|_, w| w.uif().clear_bit()) };
+    unsafe { (*stm32f303::TIM2::ptr()).sr.modify(|_, w| w.uif().clear_bit()) };
 
     // Read ODR8 to see if the pin is set, and if so, clear it,
     // otherwise, set it. We use the atomic BSRR register to
     // set/reset it without needing to read-modify-write ODR.
-    let ptr = stm32f405::GPIOA::ptr();
+    let ptr = stm32f303::GPIOE::ptr();
     unsafe {
         if (*ptr).odr.read().odr8().is_high() {
             (*ptr).bsrr.write(|w| w.br8().set_bit());
